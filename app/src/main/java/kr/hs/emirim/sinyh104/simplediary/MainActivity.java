3eab47071ba745a17a5bb493b193a170f8078b1f
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-10-18 19:43:16 +0900

    #4 button click Event Programming

--- app/src/main/java/kr/hs/emirim/sinyh104/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/simplediary/MainActivity.java
@@ -1,13 +1,17 @@
 package kr.hs.emirim.sinyh104.simplediary;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
@@ -57,8 +61,29 @@ public class MainActivity extends AppCompatActivity {
                 butSave.setEnabled(true);
             }
         });
-
+        butSave.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream out=openFileOutput(fileName, Context.MODE_WORLD_WRITEABLE);
+                    String diaryContents=editDiary.getText().toString();
+                    out.write(diaryContents.getBytes());
+                    out.close();
+                    Toast.makeText(getApplicationContext(),"저장이 완료됨",Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
     String readDiary(String fileName) throws FileNotFoundException {
         String diaryContents=null;
         try {
